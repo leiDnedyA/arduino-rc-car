c69344e2db0b7fe21df7dc2909f58588c4074047
leiDnedyA <aydendiel@gmail.com>
2024-06-29 20:02:49 -0400

    feat: made upload script better

--- upload.sh
+++ upload.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
-# Compiles & uploads the sketch
+usage() {
+  echo "Usage: $0 <board port> [--monitor]"
+  exit 1
+}
 
-if [ $# -eq 0 ]; then
-    echo "No board port argument provided."
-    echo "To find your board's port, run 'arduino-cli board port'"
-    echo 'Example: "./upload.sh /dev/ttyACM0"'
-    exit 1
-fi
+BOARD=$1
+shift # Remove BOARD from the arg list
+
+MONITOR=false
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    --monitor)
+      MONITOR=true
+      shift # Remove the current argument
+      ;;
+    *)
+      echo "Invalid option: $1" >&2
+      usage
+      ;;
+  esac
+done
 
 arduino-cli compile --fqbn arduino:avr:uno .
-arduino-cli upload -p $1 --fqbn arduino:avr:uno .
+if [ $? -eq 1 ]; then
+  echo "Build failed :("
+  exit 1
+fi
+arduino-cli upload -p $BOARD --fqbn arduino:avr:uno .
+if [ $? -eq 1 ]; then
+  echo "Upload failed :("
+  exit 1
+fi
+
+if [ "$MONITOR" = true ]; then
+  arduino-cli monitor -p $BOARD
+fi
 
 exit 0
