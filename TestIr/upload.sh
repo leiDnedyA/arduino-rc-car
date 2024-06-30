9a94aac0e9b17b64ce725a9c3c2c1b18919a93bb
leiDnedyA <aydendiel@gmail.com>
2024-06-29 20:30:10 -0400

    feat: added test and clear sketches
    TestIr prints the hex values of IR signals, and clear clears the board

---
+++ TestIr/upload.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+usage() {
+  echo "Usage: $0 <board port> [--monitor]"
+  exit 1
+}
+
+# BOARD=$1
+# shift # Remove BOARD from the arg list
+BOARD="/dev/ttyACM0"
+
+MONITOR=false
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    --monitor)
+      MONITOR=true
+      shift # Remove the current argument
+      ;;
+    *)
+      echo "Invalid option: $1" >&2
+      usage
+      ;;
+  esac
+done
+
+arduino-cli compile --fqbn arduino:avr:uno .
+if [ $? -eq 1 ]; then
+  echo "Build failed :("
+  exit 1
+fi
+arduino-cli upload -p $BOARD --fqbn arduino:avr:uno .
+if [ $? -eq 1 ]; then
+  echo "Upload failed :("
+  exit 1
+fi
+
+if [ "$MONITOR" = true ]; then
+  arduino-cli monitor -p $BOARD
+fi
+
+exit 0
